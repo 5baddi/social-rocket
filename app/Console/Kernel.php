c2f5692bfb88fc0b7bad5589378bd8a155cabb38
5baddi <project@baddi.info>
2021-05-21 13:46:03 +0100

    Fix namespace

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use BADDIServices\SocialRocket\Console\Commands\Shopify\SyncOrders as ShopifySyncOrders;
+use BADDIServices\SocialRocket\Console\Commands\Shopify\SyncOrders;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        ShopifySyncOrders::class,
+        SyncOrders::class,
     ];
 
     /**
