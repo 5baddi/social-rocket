594d3703b98d06274149f1d5fd0bf0ae85cc6fcc
5baddi <project@baddi.info>
2021-05-25 09:45:23 +0100

    Update favicon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>{{ config('app.name') }}</title>
-        <link rel="icon" type="image/png" href="{{ asset('img/mini-logo.png') }}"/>
+        <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.mini.png') }}"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
         <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"/>
