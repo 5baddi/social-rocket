8a9973753da48994a279afbc2d2714614f4dc015
5baddi <project@baddi.info>
2021-05-26 14:20:06 +0100

    Updating style

--- resources/views/partials/dashboard/menu.blade.php
+++ resources/views/partials/dashboard/menu.blade.php
@@ -1,11 +1,11 @@
-<aside class="navbar navbar-vertical navbar-expand-lg navbar-transparent">
+<aside class="navbar navbar-vertical navbar-expand-lg navbar-dark">
     <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
         <span class="navbar-toggler-icon"></span>
       </button>
       <h1 class="navbar-brand navbar-brand-autodark">
         <a href="{{ route('dashboard') }}">
-          <img src="{{ asset('assets/img/logo.png') }}" width="110" height="32" alt="{{ config('app.name') }}" class="navbar-brand-image"/>
+          <img src="{{ asset('assets/img/logo-white.png') }}" width="110" height="32" alt="{{ config('app.name') }}" class="navbar-brand-image"/>
         </a>
       </h1>
       <div class="collapse navbar-collapse" id="navbar-menu">
