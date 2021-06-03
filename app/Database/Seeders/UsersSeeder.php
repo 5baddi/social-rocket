83d47ec37747ce0cb169a5a9e336997bba3062fa
5baddi <project@baddi.info>
2021-06-03 22:06:15 +0100

    Users seeder

---
+++ app/Database/Seeders/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Social Rocket
+ *
+ * @copyright   Copyright (c) 2021, BADDI Services. (https://baddi.info)
+ */
+
+namespace BADDIServices\SocialRocket\Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            User::FIRST_NAME_COLUMN     =>  "webmaster",
+            User::LAST_LOGIN_COLUMN     =>  "trysocialrocket",
+            User::EMAIL_COLUMN          =>  "webmaster@trysocialrocket.com",
+            User::PASSWORD_COLUMN       =>  "trysocialrocket@test2021",  
+            User::IS_SUPERADMIN_COLUMN  =>  true
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use BADDIServices\SocialRocket\Database\Seeders\PacksSeeder;
 use BADDIServices\SocialRocket\Database\Seeders\SettingsSeeder;
+use BADDIServices\SocialRocket\Database\Seeders\UsersSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UsersSeeder::class,
             PacksSeeder::class,
             SettingsSeeder::class,
         ]);
