1157e92136a60bc346503f3c51c8bcfccc01efb6
5baddi <project@baddi.info>
2021-07-18 18:31:43 +0100

    Update seeder

--- app/Database/Seeders/SettingsSeeder.php
+++ app/Database/Seeders/SettingsSeeder.php
@@ -23,8 +23,8 @@ class SettingsSeeder extends Seeder
         AppSetting::create([
             AppSetting::SUPPORT_EMAIL         =>  'info@clnkgo.com',
             AppSetting::INSTAGRAM_USERNAME    =>  'clnkgo',
-            AppSetting::TWITTER_USERNAME      =>  'TrySocialRocket',
-            AppSetting::FACEBOOK_USERNAME     =>  'Trysocialrocketapp-103039265306555',
+            AppSetting::TWITTER_USERNAME      =>  'clnkgo',
+            AppSetting::FACEBOOK_USERNAME     =>  'clnkgo',
         ]);
     }
 }
