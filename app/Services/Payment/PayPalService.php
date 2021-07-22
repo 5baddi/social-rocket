4f62f7b7cf91ff6886e69b33cc899fa7d3a38f2e
5baddi <project@baddi.info>
2021-07-22 21:44:51 +0100

    Init paypal service

---
+++ app/Services/Payment/PayPalService.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ClnkGO
+ *
+ * @copyright   Copyright (c) 2021, BADDI Services. (https://baddi.info)
+ */
+
+namespace BADDIServices\ClnkGO\Services\Payment;
+
+use BADDIServices\ClnkGO\Services\Service;
+
+class PayPalService extends Service
+{
+
+}
