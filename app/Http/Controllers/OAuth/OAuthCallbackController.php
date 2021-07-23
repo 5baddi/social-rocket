14c5060467b896fb60e3e3a115019ae21bce49f1
5baddi <project@baddi.info>
2021-07-23 21:41:04 +0100

    Login after link account

--- app/Http/Controllers/OAuth/OAuthCallbackController.php
+++ app/Http/Controllers/OAuth/OAuthCallbackController.php
@@ -9,12 +9,19 @@
 namespace BADDIServices\SocialRocket\Http\Controllers\OAuth;
 
 use Throwable;
+use Carbon\Carbon;
+use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
 use BADDIServices\SocialRocket\AppLogger;
 use BADDIServices\SocialRocket\Models\OAuth;
 use BADDIServices\SocialRocket\Models\Store;
+use BADDIServices\SocialRocket\Entities\Alert;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
+use BADDIServices\SocialRocket\Events\WelcomeMail;
+use BADDIServices\SocialRocket\Services\UserService;
 use BADDIServices\SocialRocket\Services\StoreService;
 use BADDIServices\SocialRocket\Services\ShopifyService;
 use BADDIServices\SocialRocket\Http\Requests\OAuthCallbackRequest;
@@ -27,10 +34,18 @@ class OAuthCallbackController extends Controller
     /** @var StoreService */
     private $storeService;
 
-    public function __construct(ShopifyService $shopifyService, StoreService $storeService)
+    /** @var UserService */
+    private $userService;
+
+    public function __construct(
+        ShopifyService $shopifyService, 
+        StoreService $storeService,
+        UserService $userService
+    )
     {
         $this->shopifyService = $shopifyService;
         $this->storeService = $storeService;
+        $this->userService = $userService;
     }
     
     public function __invoke(OAuthCallbackRequest $request)
@@ -54,24 +69,63 @@ class OAuthCallbackController extends Controller
             $oauth = $this->storeService->updateStoreOAuth($store, $attributes);
             abort_unless($oauth instanceof OAuth, Response::HTTP_BAD_REQUEST, 'Something going wrong during authentification');
 
-            $this->storeService->updateConfigurations($store);
+            $store = $this->storeService->updateConfigurations($store);
             $this->storeService->enableStore($store);
 
+            $user = $this->userService->findByEmail($store->email);
+            if (!$user instanceof User) {
+                $firstName = ucwords($store->name);
+                $lastName = 'Admin';
+
+                $shopOwner = explode(' ', $store->shop_owner);
+                if (sizeof($shopOwner) === 2) {
+                    $firstName = ucfirst($shopOwner[0]);
+                    $lastName = ucfirst($shopOwner[1]);
+                }
+
+                $user = $this->userService->create(
+                    $store,
+                    [
+                        User::FIRST_NAME_COLUMN    => $firstName,
+                        User::LAST_NAME_COLUMN     => $lastName,
+                        User::EMAIL_COLUMN         => $store->email,
+                        User::PHONE_COLUMN         => $store->phone
+                    ]
+                );
+            }
+
+            Event::dispatch(new WelcomeMail($store, $user));
+
+            $authenticateUser = Auth::loginUsingId();
+            if (!$authenticateUser) {
+                return redirect()
+                    ->route('signin')
+                    ->with('error', 'Something going wrong with the authentification');
+            }
+
+            $this->userService->update($user, [
+                User::LAST_LOGIN_COLUMN    =>  Carbon::now()
+            ]);
+
             return redirect()
-                        ->route('signup', ['store' => $store->id]);
+                ->route('dashboard')
+                ->with(
+                    'alert',
+                    new Alert('Your store has been linked successfully', 'success')
+                );
         } catch (ValidationException $ex) {
             AppLogger::setStore($store ?? null)->error($ex, 'store:oauth-callback', $request->all());
             
             return redirect()
-                        ->route('connect')
-                        ->withInput()
-                        ->withErrors($ex->errors());
+                ->route('connect')
+                ->withInput()
+                ->withErrors($ex->errors());
         } catch (Throwable $ex) {
             AppLogger::setStore($store ?? null)->error($ex, 'store:oauth-callback', $request->all());
             
             return redirect()
-                        ->route('connect')
-                        ->with('error', 'Internal server error');
+                ->route('connect')
+                ->with('error', 'Internal server error');
         }
     }
 }
