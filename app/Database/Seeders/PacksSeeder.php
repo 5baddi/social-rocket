459e7f753e4d3c0382b8e51b2e06d1593f23fb48
5baddi <project@baddi.info>
2021-07-28 21:55:30 +0100

    Update packs seeder

--- app/Database/Seeders/PacksSeeder.php
+++ app/Database/Seeders/PacksSeeder.php
@@ -21,51 +21,10 @@ class PacksSeeder extends Seeder
     public function run()
     {
         Pack::create([
-            'name'              =>  'gravity',
-            'price'             =>  9.99,
-            'type'              =>  Pack::TYPES[0],
-            'symbol'            =>  '$',
-            'currency'          =>  'usd',
-            'features'          =>  [
-                [
-                    'key'       =>  Pack::UNLIMITED_AFFILIATES,
-                    'name'      =>  'unlimited affiliates',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::PAYOUT_METHODS,
-                    'name'      =>  '5 payout methods',
-                    'enabled'   =>  true,
-                    'limit'     =>  5,
-                ],
-                [
-                    'key'       =>  Pack::REPORTING,
-                    'name'      =>  'dashboard And data reports',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::CUSTOMIZATION,
-                    'name'      =>  'full customization',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::SUPPORT,
-                    'name'      =>  'live chat support',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::REVENUE_NOT_SHARED,
-                    'name'      =>  '100% of revenue is your\'s',
-                    'enabled'   =>  true,
-                ],
-            ]
-        ]);
-        
-        Pack::create([
             'name'          =>  'Asteroid',
             'price'         =>  10,
             'type'          =>  Pack::TYPES[1],
-            'is_popular'    =>  true,
+            'is_popular'    =>  false,
             'features'      =>  [
                 [
                     'key'       =>  Pack::UNLIMITED_AFFILIATES,
@@ -97,6 +56,48 @@ class PacksSeeder extends Seeder
                     'key'       =>  Pack::REVENUE_NOT_SHARED,
                     'name'      =>  '100% of revenue is your\'s',
                     'enabled'   =>  false,
+                ],
+            ]
+        ]);
+
+        Pack::create([
+            'name'              =>  'gravity',
+            'price'             =>  9.99,
+            'type'              =>  Pack::TYPES[0],
+            'symbol'            =>  '$',
+            'currency'          =>  'usd',
+            'is_popular'        =>  true,
+            'features'          =>  [
+                [
+                    'key'       =>  Pack::UNLIMITED_AFFILIATES,
+                    'name'      =>  'unlimited affiliates',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::PAYOUT_METHODS,
+                    'name'      =>  '5 payout methods',
+                    'enabled'   =>  true,
+                    'limit'     =>  5,
+                ],
+                [
+                    'key'       =>  Pack::REPORTING,
+                    'name'      =>  'dashboard And data reports',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::CUSTOMIZATION,
+                    'name'      =>  'full customization',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::SUPPORT,
+                    'name'      =>  'live chat support',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::REVENUE_NOT_SHARED,
+                    'name'      =>  '100% of revenue is your\'s',
+                    'enabled'   =>  true,
                 ],
             ]
         ]);
