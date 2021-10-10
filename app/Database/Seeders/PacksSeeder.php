66a34ef85b1114775e093e6d62a3d7169f8d58cd
Mohamed Baddi <project@baddi.info>
2021-10-10 11:56:38 +0100

    Update packs seeder

--- app/Database/Seeders/PacksSeeder.php
+++ app/Database/Seeders/PacksSeeder.php
@@ -22,7 +22,7 @@ class PacksSeeder extends Seeder
     {
         Pack::create([
             'name'              =>  'starter',
-            'price'             =>  4.99,
+            'price'             =>  2.25,
             'type'              =>  Pack::RECURRING_TYPE,
             'symbol'            =>  '$',
             'currency'          =>  'usd',
@@ -65,6 +65,50 @@ class PacksSeeder extends Seeder
                 ]
             ]
         ]);
+
+        Pack::create([
+            'name'          =>  'Asteroid',
+            'price'         =>  10,
+            'type'          =>  Pack::USAGE_TYPE,
+            'features'      =>  [
+                [
+                    'key'       =>  Pack::UNLIMITED_AFFILIATES,
+                    'name'      =>  'unlimited affiliates',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::REPORTING,
+                    'name'      =>  'dashboard And data reports',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::SUPPORT,
+                    'name'      =>  'live chat support',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::PAYOUT_METHODS,
+                    'name'      =>  '5 payout methods',
+                    'enabled'   =>  true,
+                    'limit'     =>  5,
+                ],
+                [
+                    'key'       =>  Pack::REVENUE_NOT_SHARED,
+                    'name'      =>  '100% of revenue is your\'s',
+                    'enabled'   =>  false,
+                ],
+                [
+                    'key'       =>  Pack::MULTIPLE_STORES,
+                    'name'      =>  'multiple stores',
+                    'enabled'   =>  true,
+                ],
+                [
+                    'key'       =>  Pack::CUSTOMIZATION,
+                    'name'      =>  'full customization',
+                    'enabled'   =>  true,
+                ]
+            ]
+        ]);
         
         Pack::create([
             'name'              =>  'gravity',
@@ -99,50 +143,6 @@ class PacksSeeder extends Seeder
                     'key'       =>  Pack::REVENUE_NOT_SHARED,
                     'name'      =>  '100% of revenue is your\'s',
                     'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::MULTIPLE_STORES,
-                    'name'      =>  'multiple stores',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::CUSTOMIZATION,
-                    'name'      =>  'full customization',
-                    'enabled'   =>  true,
-                ]
-            ]
-        ]);
-        
-        Pack::create([
-            'name'          =>  'Asteroid',
-            'price'         =>  10,
-            'type'          =>  Pack::USAGE_TYPE,
-            'features'      =>  [
-                [
-                    'key'       =>  Pack::UNLIMITED_AFFILIATES,
-                    'name'      =>  'unlimited affiliates',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::REPORTING,
-                    'name'      =>  'dashboard And data reports',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::SUPPORT,
-                    'name'      =>  'live chat support',
-                    'enabled'   =>  true,
-                ],
-                [
-                    'key'       =>  Pack::PAYOUT_METHODS,
-                    'name'      =>  '5 payout methods',
-                    'enabled'   =>  true,
-                    'limit'     =>  5,
-                ],
-                [
-                    'key'       =>  Pack::REVENUE_NOT_SHARED,
-                    'name'      =>  '100% of revenue is your\'s',
-                    'enabled'   =>  false,
                 ],
                 [
                     'key'       =>  Pack::MULTIPLE_STORES,
