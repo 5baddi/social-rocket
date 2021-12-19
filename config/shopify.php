316561e5352a72ca21392761a731110fd43fcebd
Mohamed Baddi <project@baddi.info>
2021-12-19 17:38:57 +0100

    Update shopify keys

--- config/shopify.php
+++ config/shopify.php
@@ -9,5 +9,5 @@
 return [
     'api_key'                   =>  env('SHOPIFY_API_KEY'),
     'client_secret'             =>  env('SHOPIFY_API_SECRET'),
-    'test_enabled'              =>  env('SHOPIFY_API_TEST_ENABLED'),
+    'test_enabled'              =>  env('SHOPIFY_API_TEST_ENABLED', false),
 ];
