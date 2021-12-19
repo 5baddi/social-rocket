2c80be0c3e9089f6a52d5b64bfb0bb1ae19fe071
Mohamed Baddi <project@baddi.info>
2021-12-19 23:27:35 +0100

    init webhook endpoint

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -8,11 +8,12 @@
 
 use BADDIServices\SocialRocket\Http\Controllers\Webhooks as Webhooks;
 
-Route::name('webhooks')
+Route::name('webhooks.')
     ->prefix('webhooks')
     ->middleware(['guest', 'is-shopify-webhook'])
     ->group(function() {
-        Route::post('/customers/data_request', Webhooks\ShowCustomerController::class)->name('.customer.show');
-        Route::post('/customers/redact', Webhooks\DeleteCustomerController::class)->name('.customer.delete');
-        Route::post('/shop/redact', Webhooks\DeleteStoreController::class)->name('.store.delete');
+        Route::any('/notify', Webhooks\ShowCustomerController::class)->name('notify');
+        Route::post('/customers/data_request', Webhooks\ShowCustomerController::class)->name('customer.show');
+        Route::post('/customers/redact', Webhooks\DeleteCustomerController::class)->name('customer.delete');
+        Route::post('/shop/redact', Webhooks\DeleteStoreController::class)->name('store.delete');
     });
